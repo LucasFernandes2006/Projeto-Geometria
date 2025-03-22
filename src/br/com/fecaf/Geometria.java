12046a08e12e36a483fc35ff671fec45a581f1c4
LucasFernandes2006 <lucasdasilvafernandes27@gmail.com>
2025-03-22 20:18:32 -0300

    atividade

---
+++ src/br/com/fecaf/Geometria.java
@@ -0,0 +1,12 @@
+package br.com.fecaf;
+
+import br.com.fecaf.controller.Menu;
+
+public class Geometria {
+    public static void main(String[] args) {
+        //Instância um novo Menu
+        Menu menu = new Menu();
+        //Chama a função de menu
+        menu.executarMenu();
+    }
+}
